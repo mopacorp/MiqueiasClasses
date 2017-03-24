f101bfbd8f1254c80fdb4c6a4b38923b032e1e3c
Lucas Machado <alenda132@gmail.com>
2017-03-24 17:43:56 -0300

    Esqueci o ( ; ) Copia e cola esse banco em um banco novo q funciona tranquilo, antes tinha copiar e colar.tabela por tabela

--- banco.sql
+++ banco.sql
@@ -5,7 +5,7 @@ CREATE TABLE `cliente` (
   email VARCHAR(50) NOT NULL,
   senha VARCHAR(45) NOT NULL,
   situacao INT default 1
-  )
+  );
 
 CREATE TABLE `empresa` (
   id INT PRIMARY KEY AUTO_INCREMENT,
@@ -13,7 +13,7 @@ CREATE TABLE `empresa` (
   cnpj VARCHAR(45) NOT NULL,
   longitude FLOAT NOT NULL,
   latitude FLOAT NOT NULL
-  )
+  );
 
 
 CREATE TABLE `contato` (
@@ -22,7 +22,7 @@ telefone VARCHAR(20),
 email varchar(255),
 site varchar(255),
 FOREIGN KEY (id_empresa) REFERENCES empresa(id)
-)
+);
 
 CREATE TABLE `cliente_empresa` (
 id_cliente int, 
@@ -30,7 +30,7 @@ id_empresa int,
 avaliacao double (2,2),
 FOREIGN KEY (id_cliente) REFERENCES cliente(id),
 FOREIGN KEY (id_empresa) REFERENCES empresa(id)
-)
+);
 
 CREATE TABLE `compra` (
 id int PRIMARY KEY AUTO_INCREMENT,
@@ -39,7 +39,7 @@ id_empresa int,
 situacao int default 1,
 FOREIGN KEY (id_cliente) REFERENCES cliente(id),
 FOREIGN KEY (id_empresa) REFERENCES empresa(id)
-)
+);
 
 CREATE TABLE `categoria` (
 id int PRIMARY KEY AUTO_INCREMENT,	
@@ -53,7 +53,7 @@ id int PRIMARY KEY AUTO_INCREMENT,
 nome varchar(250),
 id_categoria int,
 FOREIGN KEY (id_categoria) REFERENCES categoria(id)
-)
+);
 
 CREATE TABLE `produto_empresa` (
 id_empresa int, 
@@ -63,7 +63,7 @@ descricao VARCHAR(255),
 situacao int default 1,
 FOREIGN KEY (id_empresa) REFERENCES empresa(id),
 FOREIGN KEY (id_produto) REFERENCES produto(id)
-)
+);
 
 CREATE TABLE `venda_produto` (
 id int PRIMARY KEY AUTO_INCREMENT,
@@ -75,5 +75,5 @@ motivo VARCHAR(255) DEFAULT "",
 observacao VARCHAR(255),/*Observação dentro do pedido, ex: Coca ja com o canudo ou pizza sem bordas e pouco pimentão*/
 FOREIGN KEY (id_produto) REFERENCES produto_empresa(id_produto),
 FOREIGN KEY (id_compra) REFERENCES compra(id)
-)
+);
 
